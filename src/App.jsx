06d7300aa2ad5bb99c961657b294b753cb1d2ae1
Sumitpal0572 <asmyselfsumit@gmail.com>
2024-12-07 23:06:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/LoginPage.jsx";
+import Signup from "./pages/SignupPage.jsx";
+import Products from "./pages/ProductsPage.jsx";
+import Navbar from "./components/Navbar.jsx";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          {/* Route for Home Page */}
+          <Route path="/" element={<h1>Welcome to the Home Page</h1>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
